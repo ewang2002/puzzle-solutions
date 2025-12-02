42d64670a0bf32b9ad2ada9d93299e534c5a4f96
ewang2002 <37031713+ewang2002@users.noreply.github.com>
2025-12-01 22:17:17 -0800

    aoc(2025): finish day 2

---
+++ aoc/rust/aoc2025/src/aoc/day02.rs
@@ -0,0 +1,78 @@
+use common::numbers::{join_digits, to_digits};
+use common::problem::day::{AoCProblem, Solution};
+
+pub struct Day02 {
+    product_id_ranges: Vec<(usize, usize)>,
+}
+
+impl AoCProblem for Day02 {
+    fn prepare(input: String) -> Self {
+        Self {
+            product_id_ranges: input
+                .split(',')
+                .map(|range| range.split_once('-').unwrap())
+                .map(|(l, r)| (l.parse().unwrap(), r.parse().unwrap()))
+                .collect(),
+        }
+    }
+
+    fn part1(&mut self) -> Solution {
+        self.product_id_ranges
+            .iter()
+            .map(|(min, max)| {
+                (*min..=*max)
+                    .map(to_digits)
+                    // For the ID to be invalid, the left-half and right-half needs to be the same
+                    // so the length needs to be even so we can evenly divide the two halves.
+                    .filter(|d| d.len() % 2 == 0 && d[..d.len() / 2] == d[d.len() / 2..])
+                    .map(|d| join_digits(&d))
+                    .sum::<usize>()
+            })
+            .sum::<usize>()
+            .into()
+    }
+
+    fn part2(&mut self) -> Solution {
+        fn is_invalid_id(num: usize) -> bool {
+            let digits = to_digits(num);
+
+            // Get all multiples of digits.len() except for digits.len() itself since we want
+            // to evenly divide up the number into multiples to check for equality
+            let multiples_of_len = (1..digits.len())
+                .filter(|l| digits.len().is_multiple_of(*l))
+                .collect::<Vec<_>>();
+
+            // now that we have all multiples, go through them and see if we can find any
+            // repeating digits
+            for multiple in multiples_of_len {
+                let base = join_digits(&digits[..multiple]);
+                // Skip the first chunk because that's the same as the base
+                if digits
+                    .chunks(multiple)
+                    .skip(1)
+                    .all(|c| join_digits(c) == base)
+                {
+                    return true;
+                }
+            }
+
+            false
+        }
+
+        self.product_id_ranges
+            .iter()
+            .map(|(min, max)| (*min..=*max)
+                .filter(|n| is_invalid_id(*n))
+                .sum::<usize>())
+            .sum::<usize>()
+            .into()
+    }
+
+    fn day() -> u32 {
+        2
+    }
+
+    fn year() -> u32 {
+        2025
+    }
+}
--- aoc/rust/common/src/numbers.rs
+++ aoc/rust/common/src/numbers.rs
@@ -1,3 +1,50 @@
+/// Joins the given list of digits into a single number.
+///
+/// # Parameters
+/// - `digits`: The digits.
+///
+/// # Returns
+/// The number corresponding to the digits given.
+pub fn join_digits(digits: &[u32]) -> usize {
+    let mut num = 0;
+    for digit in digits {
+        assert!(
+            (0..10).contains(digit),
+            "digit should be in range [0, 9], but is actually {digit}"
+        );
+        num = num * 10 + digit;
+    }
+
+    num as usize
+}
+
+/// Converts the given number into a list of digits. This doesn't handle negative
+/// numbers, so you'll need to provide the absolute value yourself.
+///
+/// # Parameters
+/// - `n`: The number to get the digits for.
+///
+/// # Returns
+/// The digits.
+pub fn to_digits<T>(n: T) -> Vec<u32>
+where
+    T: Into<usize>,
+{
+    let mut n = n.into();
+    if n == 0 {
+        return vec![0];
+    }
+
+    let mut digits = vec![];
+    while n > 0 {
+        digits.push((n % 10) as u32);
+        n /= 10;
+    }
+
+    digits.reverse();
+    digits
+}
+
 /// Computes the GCD of two integers.
 ///
 /// # Parameters
